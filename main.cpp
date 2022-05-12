5e17e1a0a3043d836ef83c348b332467229abb75
Ben Miller <45132566+gytanzo@users.noreply.github.com>
2022-05-12 18:16:04 -0400

    Reverse Pendulum

--- main.cpp
+++ main.cpp
@@ -6,15 +6,17 @@ Date last updated: 4/15/2022
 Function: Control the overall function of the reverse pendulum
 Modules: N/A
 */
+#include <cstdio>
 #include <mbed.h>
 #include <iostream>
 #include <iomanip>
 #include <cstdlib>
-#include <servo.h>
+#include <Servo.h>
+#include <string.h>
 
-DigitalOut trigger(PA_8);
+DigitalOut trigger(PA_1);
 DigitalIn IR(PB_3);
-InterruptIn echo(PA_9);
+InterruptIn echo(PA_2);
 Timer timer; 
 
 int checkDistance(void) {
@@ -41,19 +43,51 @@ int checkDistance(void) {
   //Calculate the distance from the time
   store = timer.read_us();
   store = store * 0.0340 / 2;
+
+
+
   //Finally return the distance of the object from sensor.
+  if(store > 100 && store <= 1004){
+      printf("%d BIG NUMBER INCOMING: \n", store);
+      return store;
+  }
+
+  else if (store > 100 && store >= 1005){
+      return 3;
+  }
+
+  else if(store > 25){
+      return 25;
+  }
+  
   return store;
 }
 
-int checkIR()
-{
-  // grab value of the ir sensor
-  // convert to digital
-  // poll every 200 ms and average the value? adjust the time value as necessary
+// int checkIR()
+// {
+//   // grab value of the ir sensor
+//   // convert to digital
+//   // poll every 200 ms and average the value? adjust the time value as necessary
+  
+//   //Wait for echo to change to high
+//   while (IR.read() == 0) {
+//       ;
+//   }
+//   //Start the timer and let it run until echo is 0 again.
+//   timer.reset();
+//   timer.start();
 
-  return IR.read();
+//   //Wait for echo to change to low
+//   while (IR.read() == 1) {
+//       ;
+//   }
+//   timer.stop();
+//   int state;
+//   //Calculate the distance from the time
+//   state = timer.read_us();
+//   return state;
 
-}
+// }
 
 int main(int argc, char *argv[])
 {
@@ -64,44 +98,77 @@ int main(int argc, char *argv[])
   //Output: PA_7 (pwm) -> SERVO
   Servo servo(PA_7);
 
-  //Input: IR sensor (PB_8)
+  //Input: IR sensor (PB_3)
   IR.mode(PullNone);
 
-  //Distance sensor (PB_9) - Initialized above
+  //Distance sensor (PA_1/PA_2) - Initialized above
+
+  //servo.calibrate(.001, 45);
+
+  int positionAngle = 0;
+  trigger=0;
   
+
   while(true) {
   // begin infinite polling for the sensors
     // update position based on results from sensors
     // i.e. if sensors indicate the ball is further from the servo, move the pendulum down
     // this part is probably functionality in a subfunction
 
-    int dist = 0; // checkDistance(); // returned in millimeters
-    int ir = 0; // checkIR(); // 0 = IR detected somthing. 1 = nothing detected.
+    int dist = checkDistance(); // returned in millimeters
+    while(dist >= 100){
+        dist = checkDistance();
+    }
+    printf("%d\n", dist);
+    // int ir = checkIR(); // 0 = IR detected somthing. 1 = nothing detected.
     
-    servo.position(-90);
-    wait_us(5000000);
-    servo.position(90);
-    wait_us(5000000);
+    
+    // Default position for testing purposes
+    //servo.position(0);
+    //wait_us(5000000);
 
 
+    // positionAngle = -15;
 
-    if(dist<50 && ir==1){
-        //distance sensor and IR is on the left
-        //tilt servo right 
-        //ball is on the left side (close side)
-
+    if (3 <= dist && dist <= 6){
+        // formula 1 (A into B)
+        // -45 to -30
+        positionAngle = 25/2*dist - 165/2;
     }
-    else if(dist > 50 && ir==0){
-        //distance sensor and IR is on the left
-        //tilt servo left 
-        //ball is on the right side (far side)
 
+    else if (7 <= dist && dist <= 10){
+        // formula 2 (B into C)
+        // -30 to -20
+        positionAngle = 5*dist - 80;
     }
+
+    else if (11 <= dist && dist <= 17){
+        // formula 3 (C, balance)
+        // -20 to -15 to -10
+        positionAngle = -10;
+    }
+
+    else if (18 <= dist && dist < 21){
+        // formula 4 (C into B)
+        // -10 to 0
+        positionAngle = 5*dist - 90;
+    }
+
+    else if (22 <= dist && dist <= 25){
+        // formula 5 (B into A)
+        // 5 to 20
+        positionAngle = 5/2*dist - 95/2;
+    }
+
+    
+
+
+    //printf("positionAngle = %d\n", positionAngle);
+    servo.position(positionAngle);
+    wait_us(20000);
+
   }
 
 
   return 0;
 }
-
-
-
